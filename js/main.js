33b2ac2c089166381d81f7851c56a2776ad18868
lizyweb <147726740+lizyweb@users.noreply.github.com>
2023-11-21 18:01:17 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,110 @@
+(function ($) {
+    "use strict";
+
+    // Initiate the wowjs
+    new WOW().init();
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+})(jQuery);
+
+/**
+   * Animation on scroll
+   */
+window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+        if ($('#spinner').length > 0) {
+            $('#spinner').removeClass('show');
+        }
+    }, 1);
+};
+spinner();
+
+jQuery(function ($) {
+	'use strict';
+
+	$(document).ready(function () {
+
+		// banner-carousel
+		function bannerCarouselOne() {
+			$('.banner-carousel.banner-carousel-1').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				dots: false,
+				speed: 600,
+				arrows: true,
+				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+			});
+			$('.banner-carousel.banner-carousel-1').slickAnimation();
+		}
+		bannerCarouselOne();
+
+		// banner Carousel Two
+		function bannerCarouselTwo() {
+			$('.banner-carousel.banner-carousel-2').slick({
+				fade: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				dots: false,
+				speed: 600,
+				arrows: true,
+				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+			});
+		}
+		bannerCarouselTwo();
+
+		// pageSlider
+		function pageSlider() {
+			$('.page-slider').slick({
+				fade: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				dots: false,
+				speed: 600,
+				arrows: true,
+				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+			});
+		}
+		pageSlider();
+
+	});
+
+});
